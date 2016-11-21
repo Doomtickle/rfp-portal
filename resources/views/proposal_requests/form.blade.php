ed9c1567cd8c07dee7ea3a198c11ca3e67858e2d
Daron Adkins <daron.adkins@gmail.com>
2016-11-21 01:01:31 -0600

    add select option to create RFP

--- resources/views/proposal_requests/form.blade.php
+++ resources/views/proposal_requests/form.blade.php
@@ -1,9 +1,13 @@
 {{ csrf_field() }}
 <div class="row">
-    <div class="form-group col-md-3">
-        <label for="clientName">Client Name</label>
-        <input type="text" name="clientName" id="clientName" class="form-control"
-               value="{{ old('clientName') }}">
+    <div class="form-group col-md-4">
+        <label for="clientName">Client</label>
+        <select name="clientName" id="clientName" class="form-control">
+            <?php $clients = App\Client::all() ?>
+            @foreach($clients as $client)
+                <option value="{{ $client->name }}">{{ $client->name }}</option>
+            @endforeach
+        </select>
     </div>
     <div class="form-group col-md-3">
         <label for="clientIndustry">Client Industry</label>
@@ -18,7 +22,7 @@
 </div>
 <hr>
 <div class="row">
-    <div class="form-group col-md-9">
+    <div class="form-group col-md-10">
         <label for="basicDescription">Basic Description</label>
         <textarea type="text" name="basicDescription" id="basicDescription"
                   class="form-control" value="{{ old('basicDescription') }}"
