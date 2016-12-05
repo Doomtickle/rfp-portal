c4724922e87fab61b057f1529b36a67e656ab20e
Daron Adkins <daron.adkins@gmail.com>
2016-12-05 13:39:48 -0600

    routes

--- routes/web.php
+++ routes/web.php
@@ -32,7 +32,7 @@
 
     Route::get('/clients/all', 'ClientsController@index');
     Route::get('proposal_requests/test', 'ProposalRequestsController@test');
-    Route::get('proposal_requests/{proposalRequest}/edit', 'ProposalRequestsController@edit');
+    Route::get('proposal_requets/{proposalRequest}/edit', 'ProposalRequestsController@edit');
     Route::patch('proposal_requests/{proposalRequest}', 'ProposalRequestsController@update');
     Route::delete('proposal_requests/{proposalRequest}', 'ProposalRequestsController@destroy');
     Route::resource('proposal_requests', 'ProposalRequestsController');
