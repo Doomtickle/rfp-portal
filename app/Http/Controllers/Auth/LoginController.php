3f2a6ca0371076eaf1f2b70575d97c26732cc799
Daron Adkins <daron.adkins@gmail.com>
2016-12-18 04:46:32 -0600

    trying to make tags work

--- app/Http/Controllers/Auth/LoginController.php
+++ app/Http/Controllers/Auth/LoginController.php
@@ -27,7 +27,6 @@ class LoginController extends Controller
      */
     public function showLoginForm()
     {
-        $test_variable = 'New test variable';
         return view('adminlte::auth.login');
     }
 
