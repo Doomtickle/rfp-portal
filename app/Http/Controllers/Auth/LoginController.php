0bc1100743419c5edd2015ddb50b05ed06b1f771
Daron Adkins <daron.adkins@gmail.com>
2016-12-18 05:19:06 -0600

    oh well

--- app/Http/Controllers/Auth/LoginController.php
+++ app/Http/Controllers/Auth/LoginController.php
@@ -30,11 +30,6 @@ class LoginController extends Controller
         return view('adminlte::auth.login');
     }
 
-    public function testFunction()
-    {
-        return 'foo';
-    }
-    
 
     /**
      * Where to redirect users after login.
